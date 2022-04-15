50dd4b9a610309c0a2188e9d76e0f2b4c85fa1fd
Tonyle2001 <Tonyle200185@yahoo.com>
2022-04-15 15:44:39 -0500

    new user function

--- User.cpp
+++ User.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "User.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//constructors
+User::User(string name, string pass)
+        :username{name}, user_password{pass}
+{}
+
+
+User::User(string name)
+        :username{name}
+{}
+
+
+string User::get_username(){
+    return this-> username;
+}
+string User::get_user_password(){
+    return this-> user_password;
+}
+
+//functions
+bool User::authenticate(){
+
+    return true;// this will be replace later when I learn how to use read and write
+}
+
+bool User::check_unique(){
+
+    return true; // this will be replace later when I learn how to use read and write
+}
+
+void User::get_user_status(){
+
+    cout << "Your profile has been made!\n" << endl;
+}

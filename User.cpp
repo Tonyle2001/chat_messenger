7983871e35c1d8bb0e6c4ea997fd5b8c7d136649
Tonyle2001 <Tonyle200185@yahoo.com>
2022-04-15 22:30:34 -0500

    new user function

--- User.cpp
+++ User.cpp
@@ -5,15 +5,20 @@
 #include "User.h"
 #include <iostream>
 #include <string>
+#include <fstream>
+
 
 using namespace std;
 
 //constructors
+User::User(){}
+
+//This is used to validate existing users
 User::User(string name, string pass)
         :username{name}, user_password{pass}
 {}
 
-
+//This is to validate if username has been taken
 User::User(string name)
         :username{name}
 {}
@@ -26,6 +31,14 @@ string User::get_user_password(){
     return this-> user_password;
 }
 
+//setters
+void User::set_username(string name){
+    this->username = name;
+}
+void User::set_password(string password){
+    this->user_password = password;
+}
+
 //functions
 bool User::authenticate(){
 
@@ -34,10 +47,28 @@ bool User::authenticate(){
 
 bool User::check_unique(){
 
-    return true; // this will be replace later when I learn how to use read and write
+    ifstream check_file;
+
+    check_file.open(get_username() + ".txt");
+
+    if(check_file)
+        return true;
+    else
+        return false;
 }
 
 void User::get_user_status(){
 
-    cout << "Your profile has been made!\n" << endl;
+    ofstream make_file(get_username() + ".txt");
+
+    make_file << get_user_password() + "\n" << endl;
+
+    if(make_file){
+
+        cout << "Welcome " << get_username() << endl;
+        cout << "Your profile has been made!\n" << endl;
+    }
+
+
+    make_file.close();
 }
